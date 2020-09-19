59de67321837f316be9530b5caa6adf0f79f6cfb
Ahn-Ssu <21600402@handong.edu>
2020-09-19 16:50:10 +0900

    3주차 과제중 ^^

---
+++ Week3/form_ok.php
@@ -0,0 +1,8 @@
+<?php
+$nick =$_GET['nickFrame'];
+$gender =$_GET['gender'];
+$nick =$_GET['nickFrame'];
+$nick =$_GET['nickFrame'];
+$nick =$_GET['nickFrame'];
+$nick =$_GET['nickFrame'];
+$nick =$_GET['nickFrame'];
